95a1308fbe8257409159a08d519957b0b4dcd9f2
Dylan Rafferty <draff1800@gmail.com>
2024-03-22 22:00:52 -0400

    first commit (Spring initializr setup)

---
+++ src/main/java/com/example/springbootmysqlintegrationdemo/SpringBootMysqlIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqlintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmysqlintegrationdemo/SpringBootMysqlIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqlintegrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
